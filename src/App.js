76e5faf92a200e7ec90e4310d943edd357a7df43
Nick Trevino <nick@nicktrevino.com>
2020-11-15 17:05:53 -0700

    Add an initial screen for difficulty selection.
    This lays the groundwork for moving between screens to start
    a game.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,24 @@ const TicTacToeBoardSquare = styled.div`
   border: 1px solid #000;
 `;
 
+const DifficultySelectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 75vh;
+`;
+
+const DifficultyButton = styled.button`
+  border: 1px outset #33C;
+  border-radius: 10px;
+  background-color: #55E;
+  color: #EEE;
+  font-size: 8vh;
+  width: 50vh;
+  height: 20vh;
+`;
+
 const BOARD_MARKER = {
   // Nothing played in the space.
   _: ' ',
@@ -52,24 +70,48 @@ class TicTacToeBoard extends React.Component {
   }
 }
 
+class DifficultySelection extends React.Component {
+  render() {
+    return (
+      <DifficultySelectionContainer>
+        <h1>Select Difficulty</h1>
+        <DifficultyButton>
+          Beatable
+        </DifficultyButton>
+        <DifficultyButton>
+          Unbeatable
+        </DifficultyButton>
+      </DifficultySelectionContainer>
+    );
+  }
+}
+
+const SCREENS = {
+  DIFFICULTY_SELECTION: 0,
+};
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      board: [
-        [BOARD_MARKER.X, BOARD_MARKER.X, BOARD_MARKER.X],
-        [BOARD_MARKER.X, BOARD_MARKER.X, BOARD_MARKER.X],
-        [BOARD_MARKER.X, BOARD_MARKER.X, BOARD_MARKER.X],
-      ],
+      screen: SCREENS.DIFFICULTY_SELECTION,
     };
   }
 
   render() {
-    const { board } = this.state;
+    const { screen } = this.state;
 
-    return <TicTacToeBoard board={board} />;
+    switch (screen) {
+      case SCREENS.DIFFICULTY_SELECTION:
+        return (
+          <DifficultySelection />
+        );
+
+      default:
+        throw new Error(`Unkown screen: ${screen}`);
+    }
   }
 }
 
